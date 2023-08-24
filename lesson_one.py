9e360a4c32b26c0a42c4ff46ffa60c62295e1cb7
Dimitri Samosiuk <imsweet75@gmail.com>
2023-08-24 21:55:06 +0300

    zadanie

--- lesson_one.py
+++ lesson_one.py
@@ -1,9 +1,16 @@
+import time
 
 my_token = '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'
 
 
+def fn():
 
+    res = [i for i in range(99999)]
 
+    return res
+print(fn())
 
-print'privet'
+start = time.time()
+time.slip
+
 
